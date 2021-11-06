0a77776932ee743181eace2167b1364915398c82
ByDexter <46813962+ByDexterTR@users.noreply.github.com>
2021-11-06 19:37:13 +0300

    Add files via upload

---
+++ loader.lua
@@ -0,0 +1,69 @@
+local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/MaterialLua/master/Module.lua"))()
+
+local UI = Material.Load({
+     Title = "Script Loader",
+     Style = 2,
+     SizeX = 300,
+     SizeY = 500,
+     Theme = "Dark"
+})
+
+local Page = UI.New({
+    Title = "Developed ByDexter",
+})
+
+Page.Button({
+	Text = "Infinite Yield",
+	Callback = function()
+		 loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+    end
+})
+
+Page.Button({
+	Text = "FPS Booster",
+	Callback = function()
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/ByDexterTR/Roblox-ScriptLoader/main/fps-booster.lua'))()
+    end
+})
+
+Page.Button({
+    Text = "Anti AFK GUI",
+    Callback = function()
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/ByDexterTR/Roblox-ScriptLoader/main/anti-afk.lua'))()
+    end
+})
+
+Page.Button({
+	Text = "Pet Sim X GUI",
+	Callback = function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/not-weuz/Lua/main/petsimx.lua'))()
+    end
+})
+
+Page.Button({
+	Text = "Shampoo Simulator GUI",
+	Callback = function()
+		 loadstring(game:HttpGet('https://raw.githubusercontent.com/insanedude59/shampoosimulatorscript/main/OPENSOURCE'))()
+    end
+})
+
+Page.Button({
+	Text = "Ultimate Driving Car GUI",
+	Callback = function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/Luminential/releases/main/ac6_gui.luau", true))()
+    end
+})
+
+Page.Button({
+    Text = "Anime Fighters GUI",
+    Callback = function()
+        loadstring(game:HttpGet(('https://raw.githubusercontent.com/pspboy08/dollhouse/main/AFSGUI.lua')))()
+    end
+})
+
+Page.Button({
+    Text = "Fishing Simulator GUI",
+    Callback = function()
+		loadstring(game:HttpGet(('https://raw.githubusercontent.com/ByDexterTR/Roblox-ScriptLoader/main/fishing-simulator-gui.lua')))()
+    end
+})
