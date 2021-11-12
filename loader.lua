bf09bbe214b758a0ba751fec0edad019c2ef6dfe
ByDexter <46813962+ByDexterTR@users.noreply.github.com>
2021-11-12 21:07:12 +0200

    Magnet Masters GUI added

--- loader.lua
+++ loader.lua
@@ -101,3 +101,12 @@ Page.Button({
 		loadstring(game:HttpGet(('https://raw.githubusercontent.com/ByDexterTR/Roblox-ScriptLoader/main/thi/fishing-simulator-gui.lua')))()
     end
 })
+
+
+Page.Button({
+    Text = "Magnet Masters GUI",
+    Callback = function()
+		getgenv().CollectSpeed = 0.01
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/ShadowWalker924/ShadowWalker924/main/Main%20Magnet%20Masters%20Gui", true))()
+    end
+})
